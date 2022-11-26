686ff79cd8f1fd1aa93271d6ccc619ff0768760c
Bogach <roman.bogach@gmail.com>
2022-11-26 16:42:53 +0200

    AWS Restart Python Lab 12

---
+++ main.py
@@ -0,0 +1,31 @@
+# Python3.6
+# Coding: utf-8
+# Store the human preproinsulin sequence in a variable called preproinsulin:
+preproInsulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+# Store the remaining sequence elements of human insulin in variables:
+lsInsulin = "malwmrllpllallalwgpdpaaa"
+bInsulin = "fvnqhlcgshlvealylvcgergffytpkt"
+aInsulin = "giveqcctsicslyqlenycn"
+cInsulin = "rreaedlqvgqvelgggpgagslqplalegslqkr"
+insulin = bInsulin + aInsulin
+
+pKR = {
+    'c': 8.18,
+    'k': 10.53,
+    'h': 6.00,
+    'r': 12.48,
+    'd': 3.65,
+    'e': 4.25
+}
+
+float(insulin.count("Y"))
+seqCount = ({x: float(insulin.count(x)) for x in ['y', 'c', 'k', 'h', 'r', 'd', 'e']})
+pH = 0
+
+while pH <= 14:
+    netCharge = (
+            +(sum({x: ((seqCount[x] * (10 ** pKR[x])) / ((10 ** pH) + (10 ** pKR[x])))
+                   for x in ['k', 'h', 'r']}.values()))
+            - (sum({x: ((seqCount[x] * (10 ** pH)) / ((10 ** pH) + (10 ** pKR[x]))) for x in ['c', 'd', 'e']}.values())))
+    print('{0:.2f}'.format(pH), netCharge)
+    pH += 1
